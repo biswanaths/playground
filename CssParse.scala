5bb5b4a036b2433c8d9ea9fe98b176ac07ff1503
biswanath <biswanaths@gmail.com>
2015-10-24 07:05:50 +0530

    parsing simple css

--- CssParse.scala
+++ CssParse.scala
@@ -1,10 +1,12 @@
 import scala.util.parsing.combinator._
 
 trait Node 
+case class Empty extends Node
 case class PropsValue(value:String) extends Node
 case class Props(propsValue:Node) extends Node
 case class Names(value:String) extends Node
 case class CssClass(name:Node, prop:Node) extends Node
+case class AllClasses(name:Node, prop:Node) extends Node
 
 //case class Prop(value:Char) extends Expr
 //case class Not(expr:Expr) extends Expr
@@ -20,29 +22,22 @@ object  CssParse extends RegexParsers {
     def props: Parser[Node] = 
         "{" ~> propsValue <~ "}" ^^ { case x => Props(x) }
 
-    def names : Parser[Node] = """[^\{]*""".r ^^ { x=> Names(x) }
+    def names : Parser[Node] = 
+        """[^\{]*""".r ^^ { x=> Names(x) }
 
-    def cssClass: Parser[Node] =  names ~ props ^^ { case x ~ y => CssClass(x,y) }
+    def cssClass: Parser[Node] =  
+        names ~ props ^^ { case x ~ y => CssClass(x,y) }
 
-    //def prop: Parser[Expr] = 
-    //    """[a-z]""".r ^^ { x => Prop(x.charAt(0)) } |
-    //    "(" ~> expr <~ ")"                          
+    def allClasses: Parser[Node] = 
+        cssClass ~  allClasses ^^ { case x ~ y => AllClasses(x,y) } | 
+        cssClass ^^ { case x => AllClasses(x,Empty()) }  
 
-    //def elem: Parser[Expr] = "!" ~> elem ^^ { Not(_) } | prop 
-
-    //def expr: Parser[Expr] = 
-    //    (elem <~ "&" ) ~ elem ^^ { case x ~ y => And(x,y) } |
-    //    (elem <~ "|" ) ~ elem ^^ { case x ~ y => Or (x,y) } |
-    //    elem
-
-    def parse(s:String) = parseAll(cssClass,s) match { 
+    def parse(s:String) = parseAll(allClasses,s) match { 
         case Success(result,_) => result
         case failure: NoSuccess => scala.sys.error(failure.msg)
     }
 
-    def apply(s:String): Boolean =  { 
-        val tree = parse(s)
-        println(tree)
-        true
+    def apply(s:String): Node =  { 
+        parse(s)
     }
 }
